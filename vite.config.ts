6d53dd44374b1973d4ebe5c842d25a8e56c4a852
opteacher <opteacher@hotmail.com>
2024-05-08 15:27:37 +0800

    使用错误的端口，这里应使用svrPkg端口

--- vite.config.ts
+++ vite.config.ts
@@ -15,7 +15,7 @@ export default ({ mode }) => defineConfig({
   server: {
     proxy: {
       [`^/${project.name}/(mdl|api)`]: {
-        target: 'http://192.168.1.11:/*return project.port*/',
+        target: 'http://192.168.1.11:/*return svrPort*/',
         changeOrigin: true
       }
     }
